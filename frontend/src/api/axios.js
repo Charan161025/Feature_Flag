220efd16dd6d53af37679f044008303cdb46f7b7
Charan161025 <geethacaharan314@gmail.com>
2026-05-08 15:21:34 +0530

    updates

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+export default axios.create({
+  baseURL:
+    "https://feature-flag-49kd.onrender.com/api"
 });
-
-export default API;
